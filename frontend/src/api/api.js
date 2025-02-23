398abdb57a01d94266252bf93208210857a32369
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2025-02-23 12:03:23 +0000

    chor: deployed the backend and pasted the link in the frontend.

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api/gita';
+// const API_BASE_URL = 'http://localhost:5000/api/gita';
+const API_BASE_URL = 'https://gitaamrit-the-nectar-of-the-gita.onrender.com/api/gita';
 
 export const getAllShlokas = async () => {
   const response = await axios.get(`${API_BASE_URL}`);
